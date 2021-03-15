84eb4c27202ba0e02df0cf9f864ad75425627ac4
Yo Han Joo <yohan.arbete@gmail.com>
2021-03-16 00:07:35 +0100

    added rate limiting by IP address
    more security

--- src/main/java/com/yohanjoo/airportapi/AirportapiApplication.java
+++ src/main/java/com/yohanjoo/airportapi/AirportapiApplication.java
@@ -2,12 +2,14 @@ package com.yohanjoo.airportapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Main Application
  * @author Yo Han Joo
  * @since 0.0.1-SNAPSHOT
  */
+@EnableCaching
 @SpringBootApplication
 public class AirportapiApplication {
 
